0d5b47609bebae057106f4d9cbd8ceb7eef736ee
Zachary Michael Schillaci <zachary.michael.schillaci@cern.ch>
2021-02-16 20:49:16 +0100

    Update ID software plots

--- util/Public/IDSoftwarePubPlots.cxx
+++ util/Public/IDSoftwarePubPlots.cxx
@@ -9,67 +9,7 @@
 #include "NtupleAnalysisUtils/DefaultPlotting.h"
 #include "IDPVMNtupleAnalysis/IDPVMUtilities.h"
 
-class IDSoftwareTree : public NtupleBranchMgr{
-
-    public: 
-        IDSoftwareTree(TTree* t): 
-            NtupleBranchMgr(t), 
-            year("Year",t,this),
-            mu("Mu",t,this),
-            disk("Disk",t,this) {
-                getMissedBranches(t);
-        }
-        NtupleBranch<int>   year;
-        NtupleBranch<float> mu;
-        NtupleBranch<float> disk;
-};
-
-void normalizeByEvents(Plot<TH1D> & plot) {
-    plot.populate();
-
-    const std::vector<std::pair<float, int>> nEventsByMu {
-        {16,	2969},
-        {20,	2749},
-        {25,	2486},
-        {30,	2301},
-        {35,	2132},
-    //
-        {38,	2469},
-        {40,	2416},
-        {45,	2284},
-        {50,	2169},
-        {55,	2093},
-        {58,	2047},
-    // 
-        {17.3,  3053},
-        {20,	2922},
-        {25,	2726},
-        {30,	2557},
-        {35,	2410},
-        {40,	2290},
-        {45,	2181},
-        {48,	1909},
-    // 
-        {52,	2045},
-        {54.7,	1961},
-    };
-
-    std::cout << "--- normalizeByEvents ---" << std::endl;
-    for (int i = 1; i < plot->GetXaxis()->GetNbins() + 1; ++i) {
-        int nEvents = 0;
-        for (auto & entry : nEventsByMu) {
-            int nbin = plot->FindBin(entry.first);
-            if (i == nbin) {
-                nEvents += entry.second;
-            }
-        }
-        std::cout << "---> Pileup: " << plot->GetXaxis()->GetBinLowEdge(i) << " - " << plot->GetXaxis()->GetBinUpEdge(i) << std::endl;
-        std::cout << "---> nEvents: " << nEvents << std::endl;
-        plot->SetBinContent(i, (1 / nEvents) * plot->GetBinContent(i));
-    }
-}
-
-std::pair<std::vector<float>, std::vector<float>> getDiskVersusMu(const std::string & fname, const std::string & tname) {
+std::pair<std::vector<float>, std::vector<float>> getVectorsFromTree(const std::string & fname, const std::string & tname, const char* xbranch, const char* ybranch) {
     std::unique_ptr<TFile> myFile = std::make_unique<TFile>(fname.c_str(), "READ"); 
     
     TTree* myTree = nullptr;
@@ -79,14 +19,14 @@ std::pair<std::vector<float>, std::vector<float>> getDiskVersusMu(const std::str
         return {};
     }
 
-    float mu; 
-    float disk; 
-    myTree->SetBranchAddress("Mu", &mu);
-    myTree->SetBranchAddress("Disk", &disk);
+    float xval; 
+    float yval; 
+    myTree->SetBranchAddress(xbranch, &xval);
+    myTree->SetBranchAddress(ybranch, &yval);
 
     std::map<float, float> xy;
     for (Long64_t entry = 0; myTree->GetEntry(entry); ++entry){
-        xy.emplace(mu, disk);
+        xy.emplace(xval, yval);
     }
     myFile->Close();
     
@@ -106,123 +46,173 @@ std::pair<std::vector<float>, std::vector<float>> getRatio(std::pair<std::vector
     std::vector<float> x;
     std::vector<float> y;
     for (size_t i = 0; i < r21.first.size(); ++i) {
-        if (r21.first.at(i) == r22.first.at(i)) {
-            x.push_back(r21.first.at(i));
-            y.push_back(r22.second.at(i) / r21.second.at(i));
+        for (size_t j = 0; j < r22.first.size(); ++j) {
+            if (r21.first.at(i) == r22.first.at(j)) {
+                x.push_back(r21.first.at(i));
+                y.push_back(r22.second.at(j) / r21.second.at(i));
+                break;
+            }
         }
     }
     return std::make_pair(x, y);
 }
 
-void plotDiskVersusMu(Plot<TGraph> & r21, Plot<TGraph> & r22, Plot<TGraph> & ratio) {
-    r21.populate();
-    r22.populate();
-    ratio.populate();
-    
-    CanvasOptions canvasOpts = CanvasOptions().doLumiLabel(false);
+void plotDiskVersusMu(Plot<TGraph> & graphR21, Plot<TGraph> & graphR22, Plot<TGraph> & graphR22NoLRT, Plot<TGraph> & graphRatio, Plot<TGraph> & graphRatioNoLRT) {
+    graphR21.populate();
+    graphR22.populate();
+    graphR22NoLRT.populate();
+    graphRatio.populate();
+    graphRatioNoLRT.populate();
+
+    CanvasOptions canvasOpts = CanvasOptions();
+
+    // Get our colors
+    Int_t colR21 = TColor::GetFreeColorIndex();
+    TColor* c1 = new TColor(colR21, 0.42,0.70,0.32);
+
+    Int_t colR22Full  = TColor::GetFreeColorIndex();
+    TColor* c2 = new TColor(colR22Full, 0.12,0.47,0.71);
+
+    Int_t colR22NoLRT = TColor::GetFreeColorIndex();
+    TColor* c3 = new TColor(colR22NoLRT, 0.65,0.22,0.83);
+    //
 
     PlotUtils::MultiPadCanvas mpc = canvasOpts.prepareMultiPadCanvas();
+
+    // Upper pad plots
     mpc.getPad(0)->cd();
-
-    r21->Draw("AL*");
-    r21->SetMarkerStyle(r21.plotFormat().getMarkerStyle().value());
-    r21->GetXaxis()->SetLimits(14.5, 60.5);
-    r21->GetYaxis()->SetRangeUser(900, 3500);
-    r21->GetYaxis()->SetTitle("Disk Space [kb/event]");
-    r21->GetYaxis()->SetTitleSize(0.065);
-    r21->GetYaxis()->SetTitleOffset(0.75);
-
-    r22->Draw("SAMEL*");
-    r22->SetMarkerStyle(r22.plotFormat().getMarkerStyle().value());
-
-    mpc.getPad(1)->cd();
+    graphR21->Draw("APL");
+    graphR21->SetMarkerStyle(kFullSquare);
+    graphR21->SetMarkerColor(colR21);
+    graphR21->SetLineColor(colR21);
+    graphR21->SetLineWidth(2);
     
-    ratio->Draw("AL*");
-    ratio->SetMarkerStyle(ratio.plotFormat().getMarkerStyle().value());
-    ratio->GetXaxis()->SetLimits(14.5, 60.5);
-    ratio->GetXaxis()->SetTitle("<#mu>");
-    // ratio->GetXaxis()->SetTitle("Year [A.D.]");
-    // ratio->GetXaxis()->SetLimits(2014.75, 2018.25);
-    // ratio->GetXaxis()->SetNdivisions(4);
-    ratio->GetYaxis()->SetTitle(ratio.getLegendTitle().c_str());
-    ratio->GetXaxis()->SetTitleSize(0.075);
-    ratio->GetXaxis()->SetLabelSize(0.075);
-    ratio->GetYaxis()->SetTitleSize(0.075);
-    ratio->GetYaxis()->SetLabelSize(0.065);
-    ratio->GetYaxis()->SetTitleOffset(0.55);
+    graphR22->Draw("SAMEPL");
+    graphR22->SetMarkerStyle(kFullDotLarge);
+    graphR22->SetMarkerColor(colR22Full);
+    graphR22->SetLineColor(colR22Full);
+    graphR22->SetLineWidth(2);
+
+    graphR22NoLRT->Draw("SAMEPL");
+    graphR22NoLRT->SetMarkerStyle(kOpenCircle);
+    graphR22NoLRT->SetMarkerColor(colR22NoLRT);
+    graphR22NoLRT->SetLineColor(colR22NoLRT);
+    graphR22NoLRT->SetLineWidth(2);
+
+    graphR21->GetXaxis()->SetLimits(9, 92);
+    graphR21->GetYaxis()->SetRangeUser(-15, 500);
+
+    // Lower pad ratio
+    mpc.getPad(1)->cd();
+    graphRatio->Draw("APL");
+    graphRatio->SetMarkerStyle(kFullDotLarge);
+    graphRatio->SetMarkerColor(colR22Full);
+    graphRatio->SetLineColor(colR22Full);
+    graphRatio->SetLineWidth(2);
+
+    graphRatioNoLRT->Draw("SAMEPL");
+    graphRatioNoLRT->SetMarkerStyle(kOpenCircle);
+    graphRatioNoLRT->SetMarkerColor(colR22NoLRT);
+    graphRatioNoLRT->SetLineColor(colR22NoLRT);
+    graphRatioNoLRT->SetLineWidth(2);
+
+    graphRatio->GetXaxis()->SetLimits(9, 92);
+    graphRatio->GetYaxis()->SetRangeUser(0.195, 0.935);
 
     mpc.getCanvas()->Update();
 
-    mpc.getPad(0)->cd();
-    PlotUtils::drawLegend(std::vector<PlotUtils::LegendEntry>{
-        PlotUtils::LegendEntry(r21(), r21.getLegendTitle(), r21.getLegendOption()),
-        PlotUtils::LegendEntry(r22(), r22.getLegendTitle(), r22.getLegendOption()),
-    },
-        0.20, 0.45, 0.40, 0.65
-    );
-    canvasOpts.drawLabels({"Run-2 Data Reconstruction", "ESD Disk Space (#it{to be updated to AOD})"});
+    // Upper pad labels
+    mpc.getPad(0)->cd();    
+    canvasOpts = canvasOpts.doAtlasLabel(true).doLumiLabel(false).fontSize(24).atlasLabelX(0.04).atlasLabelY(-0.09);
+    canvasOpts.drawLabels({});
 
-    canvasOpts.SaveCanvas(mpc.getCanvas(), "DiskSpaceVersusMu");
+    canvasOpts = canvasOpts.doAtlasLabel(false).doLumiLabel(false).fontSize(22).otherLabelX(0.04).otherLabelStartY(-0.16);
+    canvasOpts.drawLabels({"#it{AOD} Disk Space (ID-only)", "Run-2 Data Reconstruction"});
+
+    PlotUtils::drawLegend(std::vector<PlotUtils::LegendEntry>{
+        PlotUtils::LegendEntry(graphR21(),      "Run-2 Reconstruction",         "PL"),
+        PlotUtils::LegendEntry(graphR22(),      "Run-3 Reconstruction",         "PL"),
+        PlotUtils::LegendEntry(graphR22NoLRT(), "Run-3 Reconstruction w/o LRT", "PL"),
+    },
+        0.20, 0.45, 0.55, 0.65
+    );
+    
+    PlotUtils::drawTLatex(0.795, 0.84, "Not on GRL", 18, 53, 11, kGray+3);
+    PlotUtils::drawLine(71, -15, 71, 500, kBlack, kDashed, 3);
+    TBox box1(71, -15, 92, 500);
+    box1.SetFillColor(kGray+2);
+    box1.SetFillStyle(3004);
+    box1.Draw();
+
+    graphR21->GetYaxis()->SetTitle("Disk Space [kb/event]");
+    graphR21->GetXaxis()->SetTitleFont(43);
+    graphR21->GetYaxis()->SetTitleFont(43);
+    graphR21->GetXaxis()->SetLabelFont(43);
+    graphR21->GetYaxis()->SetLabelFont(43);
+    graphR21->GetXaxis()->SetTitleSize(24); 
+    graphR21->GetYaxis()->SetTitleSize(24); 
+    graphR21->GetXaxis()->SetLabelSize(22); 
+    graphR21->GetYaxis()->SetLabelSize(22); 
+    
+    graphR21->GetYaxis()->SetTitleOffset(1.05);
+
+    // Lower pad labels
+    mpc.getPad(1)->cd();
+    PlotUtils::drawLine(71, 0.195, 71, 0.935, kBlack, kDashed, 3);
+    TBox box2(71, 0.195, 92, 0.935);
+    box2.SetFillColor(kGray+2);
+    box2.SetFillStyle(3004);
+    box2.Draw();
+    
+    graphRatio->GetXaxis()->SetTitle("< #mu >");
+    graphRatio->GetYaxis()->SetTitle("Run-3 / Run-2");
+    graphRatio->GetXaxis()->SetTitleFont(43);
+    graphRatio->GetYaxis()->SetTitleFont(43);
+    graphRatio->GetXaxis()->SetLabelFont(43);
+    graphRatio->GetYaxis()->SetLabelFont(43);
+    graphRatio->GetXaxis()->SetTitleSize(24); 
+    graphRatio->GetYaxis()->SetTitleSize(24); 
+    graphRatio->GetXaxis()->SetLabelSize(22); 
+    graphRatio->GetYaxis()->SetLabelSize(22); 
+
+    graphRatio->GetXaxis()->SetTitleOffset(2.25);
+    graphRatio->GetYaxis()->SetTitleOffset(1.05);
+
+    canvasOpts.SaveCanvas(mpc.getCanvas(), "Size_vs_mu_run2_vs_run3");
 }
 
 int main (int, char**) {
 
     const std::string tree  = "Squirrels";
-    const std::string inR21 = "/home/zschilla/Working/IDPVMAnalysis/run/SPOTResults.rel21Test.root";        
-    const std::string inR22 = "/home/zschilla/Working/IDPVMAnalysis/run/SPOTResults.rel22Test.root";
+    const std::string inR21 = "/home/zschilla/Working/IDPVMAnalysis/run/SPOT/v02/SPOTResults.r21-2021-02-16.root";        
+    const std::string inR22 = "/home/zschilla/Working/IDPVMAnalysis/run/SPOT/v02/SPOTResults.r22-2021-02-16.root";
 
     SetAtlasStyle();
+    // Configure branch names
+    const char* mu = "Mu";
+    const char* disk = "TotalDisk";
+    const char* LRT  = "DiskFromLRT";
 
-    // NTAU Histogram Approach
+    auto r21muDisk      = getVectorsFromTree(inR21, tree, mu, disk);
+    auto r22muDisk      = getVectorsFromTree(inR22, tree, mu, disk);
+    auto r22muDiskNoLRT = getVectorsFromTree(inR22, tree, mu, LRT);
+    for (size_t i = 0; (i < r22muDisk.first.size() && i < r22muDiskNoLRT.first.size()); ++i) {
+        if (r22muDisk.first.at(i) == r22muDiskNoLRT.first.at(i)) {
+            r22muDiskNoLRT.second.at(i) = r22muDisk.second.at(i) - r22muDiskNoLRT.second.at(i);
+        }
+    }
 
-    // Sample<IDSoftwareTree> sampleR21("sampleR21", inR21, tree);
-    // Sample<IDSoftwareTree> sampleR22("sampleR22", inR22, tree);
+    auto ratio      = getRatio(r21muDisk, r22muDisk);
+    auto ratioNoLRT = getRatio(r21muDisk, r22muDiskNoLRT);
 
-    // Selection<IDSoftwareTree> dummy("dummy", [](IDSoftwareTree &) { return true; });
-   
-    // TH1D mu("mu",";<#mu>;Disk Space [kb/event]",9,17.5,62.5);
-    // PlotFillInstructionWithRef<TH1D, IDSoftwareTree> diskMuFiller("diskMuFiller", [](TH1D* h, IDSoftwareTree &t) 
-    //     { 
-    //         int nbin       = h->FindBin(t.mu());
-    //         float content = h->GetBinContent(nbin);
-    //         h->SetBinContent(nbin, content + t.disk());
-    //     }, &mu);
+    Plot<TGraph> graphR21("graphR21", std::make_shared<TGraph>(r21muDisk.first.size(), &(r21muDisk.first[0]), &(r21muDisk.second[0])).get());
+    Plot<TGraph> graphR22("graphR22", std::make_shared<TGraph>(r22muDisk.first.size(), &(r22muDisk.first[0]), &(r22muDisk.second[0])).get());
+    Plot<TGraph> graphR22NoLRT("graphR22NoLRT", std::make_shared<TGraph>(r22muDiskNoLRT.first.size(), &(r22muDiskNoLRT.first[0]), &(r22muDiskNoLRT.second[0])).get());
 
-    // TH1D year("year",";Year [A.D];Disk Space [kb/event]",4,2015,2019);
-    // PlotFillInstructionWithRef<TH1D, IDSoftwareTree> diskYearFiller("diskYearFiller", [](TH1D* h, IDSoftwareTree &t) 
-    //     { 
-    //         int nbin       = h->FindBin(t.year());
-    //         float content = h->GetBinContent(nbin);
-    //         h->SetBinContent(nbin, content + t.disk());
-    //     }, &year);
+    Plot<TGraph> graphRatio("graphRatio", std::make_shared<TGraph>(ratio.first.size(), &(ratio.first[0]), &(ratio.second[0])).get());
+    Plot<TGraph> graphRatioNoLRT("graphRatioNoLRT", std::make_shared<TGraph>(ratioNoLRT.first.size(), &(ratioNoLRT.first[0]), &(ratioNoLRT.second[0])).get());
 
-    // Plot<TH1D> plotMuR21(sampleR21, dummy, diskMuFiller, "Rel. 21", "L", PremadePlotFormats::Lines1().ExtraDrawOpts("HIST"));
-    // Plot<TH1D> plotMuR22(sampleR22, dummy, diskMuFiller, "Rel. 22", "L", PremadePlotFormats::Lines2().ExtraDrawOpts("HIST"));
-
-    // Plot<TH1D> plotYearR21(sampleR21, dummy, diskYearFiller, "Rel. 21", "L", PremadePlotFormats::Lines1().ExtraDrawOpts("HIST"));
-    // Plot<TH1D> plotYearR22(sampleR22, dummy, diskYearFiller, "Rel. 22", "L", PremadePlotFormats::Lines2().ExtraDrawOpts("HIST"));
-
-    // PlotContent<TH1D> muPlots({plotMuR21, plotMuR22}, {"Run-2 Data Reconstruction", "AOD Disk Space"}, "DiskSpaceVersusMu", "", 
-    //     CanvasOptions().doLumiLabel(false).yMin(905).yMax(4250).ratioAxisTitle("Rel. 22 / Rel. 21").ratioMin(0.35).ratioMax(1.05).extraXtitleOffset(0.85));
-    // DefaultPlotting::draw1DWithRatio(muPlots);
-
-    // PlotContent<TH1D> yearPlots({plotYearR21, plotYearR22}, {"Run-2 Data Reconstruction", "AOD Disk Space"}, "DiskSpaceVersusYear", "", 
-    //     CanvasOptions().doLumiLabel(false).yMin(905).yMax(4250).ratioAxisTitle("Rel. 22 / Rel. 21").ratioMin(0.0).ratioMax(1.05).extraXtitleOffset(0.85));
-    // DefaultPlotting::draw1DWithRatio(yearPlots);
-
-    // ROOT TGraph Approach ('old school')
-
-    auto r21xy = getDiskVersusMu(inR21, tree);
-    auto r22xy = getDiskVersusMu(inR22, tree);
-    auto ratio = getRatio(r21xy, r22xy);
-
-    std::cout << r21xy.first.size() <<  " - " << r22xy.first.size() << std::endl;
-
-    Plot<TGraph> graphMuR21("graphMuR21", std::make_shared<TGraph>(r21xy.first.size(), &(r21xy.first[0]), &(r21xy.second[0])).get(), "Rel. 21", "PL", PremadePlotFormats::Markers1());
-    Plot<TGraph> graphMuR22("graphMuR22", std::make_shared<TGraph>(r22xy.first.size(), &(r22xy.first[0]), &(r22xy.second[0])).get(), "Rel. 22", "PL", PremadePlotFormats::Markers2());
-    Plot<TGraph> graphRatio("graphRatio", std::make_shared<TGraph>(ratio.first.size(), &(ratio.first[0]), &(ratio.second[0])).get(), "Rel. 22 / Rel. 21", "PL", PremadePlotFormats::Markers2());
-
-    plotDiskVersusMu(graphMuR21, graphMuR22, graphRatio);
+    plotDiskVersusMu(graphR21, graphR22, graphR22NoLRT, graphRatio, graphRatioNoLRT);
 
     return EXIT_SUCCESS;
 }
